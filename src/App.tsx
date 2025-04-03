3b2200351b3e6194c364988a159ee49ec6c4303c
sam0420 <samhashimi10@gmail.com>
2025-04-03 21:04:54 +0200

    added a spacer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import { Cover } from './Page/Cover'
-
+import { Spacer } from './Page/Spacer'
 import './App.css'
+
 
 function App() {
 
   return (
     <>
       <Cover/>
+      <Spacer/>
     </>
   )
 }
